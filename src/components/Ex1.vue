a218b160228fce81dae018a9e04e7eb90a6421a1
luxuan05 <luxuan.wee.2024@computing.smu.edu.sg>
2025-10-29 01:20:29 +0000

    added comments to ex 1

--- src/components/Ex1.vue
+++ src/components/Ex1.vue
@@ -19,6 +19,9 @@
    Sound level
     <input type="number" min="0" max="3" v-model="soundLevel"> 
     <!-- TODO: Add Code Here to make use of the component -->
+    <!--for static value = sound-icon level = "soundLevel"-->
+    
+    <!--this method (binding is for dynamic values)-->
     <soundIcon :soundLevel="soundLevel" />
 
 </template>
